2cac02faf9d00da37956175d685e3ae69adefe11
unknown <ajmeerachandu2001@gmail.com>
2024-07-26 22:42:19 +0530

    MongoDB setup

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import morgan from "morgan";
+import connectDB from "./config/db.js";
 
 //configure env file
 dotenv.config()
@@ -13,6 +14,10 @@ const app=express()
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
+
+//config DB
+connectDB()
+
 
 app.get('/',(req,res)=>{
     res.send("<h1>Welcome to backend</h1>")
