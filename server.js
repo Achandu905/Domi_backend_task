8c484f498c9ca70bf6eb90d56899a468179de26e
unknown <ajmeerachandu2001@gmail.com>
2024-07-26 22:23:51 +0530

    Project setup

---
+++ server.js
@@ -0,0 +1,25 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import morgan from "morgan";
+
+//configure env file
+dotenv.config()
+
+//rest object
+const app=express()
+
+//middlewares
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+app.get('/',(req,res)=>{
+    res.send("<h1>Welcome to backend</h1>")
+})
+
+const PORT=process.env.PORT || 8080
+
+app.listen(PORT,()=>{
+  console.log(`Server is running on PORT NO :${PORT} `)
+})
